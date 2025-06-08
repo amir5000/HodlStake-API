b50888c692be8cdae42b7bdab7fe1edeae8f9f4c
Amir Arafat <amirarafat1@gmail.com>
2025-06-08 13:11:09 -0600

    fixing config

--- ui/src/services/config.ts
+++ ui/src/services/config.ts
@@ -1,5 +1,7 @@
 export default {
     api: {
-        baseUrl: import.meta.env.API_URL,
+        baseUrl: import.meta.env.VITE_API_URL,
     },
 } as const
+
+console.log("import.meta.env.API_URL: ", import.meta.env.VITE_API_URL)
